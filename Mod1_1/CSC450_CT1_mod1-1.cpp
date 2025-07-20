1017bc251480971d3aa6e3ee7c0dc793f2f7ae3f
FistanRaist <jpcarn77@hotmail.com>
2025-07-20 10:20:15 -0600

    Module 1

---
+++ Mod1_1/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,24 @@
+// Jeremy Carney, CSC450, July 20th 2025
+// File: CSC450_CT1_mod1-1.cpp
+
+#include <iostream>
+#include <conio.h>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main function
+int main() 
+{
+    // Standard output statement
+    cout << "Welcome to this C++ Program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Wait for the output screen
+    cout << "Press any key to continue...";
+    getch();
+
+    // Main function return statement
+    return 0;
+}
---
+++ Mod1_2/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,28 @@
+// Jeremy Carney, CSC450, July 20th 2025
+// File: CSC450_CT1_mod1-2.cpp
+
+#include <iostream>
+#include <conio.h>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main function
+int main() 
+{
+    double myMoney = 1000.50; // This should be printed out
+
+    // Standard output statement
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait for output screen
+    cout << "Press any key to continue...";
+    getch();
+
+    // Main function return statement
+    return 0;
+}
